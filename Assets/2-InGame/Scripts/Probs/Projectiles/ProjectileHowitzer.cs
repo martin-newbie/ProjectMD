07a3a3a6d668fe0a0524f40890da2f5a4a349ed8
martin-newbie <g.martin.min@gmail.com>
2023-11-19 22:47:24 +0900

    [#update] 유탄 방향 바라보게 하기
    미분 사용해서...

--- Assets/2-InGame/Scripts/Probs/Projectiles/ProjectileHowitzer.cs
+++ Assets/2-InGame/Scripts/Probs/Projectiles/ProjectileHowitzer.cs
@@ -13,8 +13,19 @@ public class ProjectileHowitzer : ProjectileBehaviour
 
     public override void ProjectileAction(Vector3 start, Vector3 end, float t)
     {
+        transform.position = GetPositionT(start, end, t);
+
+        var pos1 = GetPositionT(start, end, t);
+        var pos2 = GetPositionT(start, end, t + 0.000001f);
+
+        float eul = (pos2.y - pos1.y) / (pos2.x - pos1.x);
+        transform.rotation = Quaternion.Euler(0, 0, eul * Mathf.Rad2Deg);
+    }
+
+    private Vector3 GetPositionT(Vector3 start, Vector3 end, float t)
+    {
         float targetX = start.x + (end.x - start.x) * t;
-        float targety = start.y + ((end.y - start.y)) * t + height * (1 - (Mathf.Abs(0.5f - t) / 0.5f) * (Mathf.Abs(0.5f - t) / 0.5f));
-        transform.position = new Vector3(targetX, targety);
+        float targetY = start.y + ((end.y - start.y)) * t + height * (1 - (Mathf.Abs(0.5f - t) / 0.5f) * (Mathf.Abs(0.5f - t) / 0.5f));
+        return new Vector3(targetX, targetY);
     }
 }
