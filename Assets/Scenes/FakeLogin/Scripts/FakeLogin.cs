c7aff287e91c8713d8f28e531d69360491947c7a
martin-newbie <g.martin.min@gmail.com>
2024-01-07 23:27:30 +0900

    [#update] 페이크 로그인 씬에 테스트 로그인 정보 보내는 시스템 추가

--- Assets/Scenes/FakeLogin/Scripts/FakeLogin.cs
+++ Assets/Scenes/FakeLogin/Scripts/FakeLogin.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 public class FakeLogin : MonoBehaviour
@@ -8,6 +9,7 @@ public class FakeLogin : MonoBehaviour
 
     private void Start()
     {
+        StartCoroutine(uploaduserdata());
         TestInitUserData();
     }
 
@@ -24,4 +26,31 @@ public class FakeLogin : MonoBehaviour
     {
         SceneManager.LoadScene("Menu");
     }
+
+    IEnumerator uploaduserdata()
+    {
+        string id = "근섭";
+        string pwd = "3333";
+        WWWForm form = new WWWForm();
+        form.AddField("input_id", id);
+        form.AddField("input_pwd", pwd);
+
+        string url = "projectmd.dothome.co.kr/phps/Temp_Project_Login.php";
+        using (UnityWebRequest request = UnityWebRequest.Post(url, form))
+        {
+            Debug.Log("보냄");
+            yield return request.SendWebRequest();
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                Debug.Log(request.downloadHandler.text);
+            }
+            Debug.Log(request.downloadHandler.text);
+
+        }
+    }
+
 }
