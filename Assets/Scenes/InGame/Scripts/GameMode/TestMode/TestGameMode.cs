47791a65013f0a639b02fb89e61ec751cd096bc3
martin-newbie <g.martin.min@gmail.com>
2024-01-26 01:54:00 +0900

    [#update] 적을 전부 잡고 게임이 끝나면 플레이어도 게임 끝내게 추가

--- Assets/Scenes/InGame/Scripts/GameMode/TestMode/TestGameMode.cs
+++ Assets/Scenes/InGame/Scripts/GameMode/TestMode/TestGameMode.cs
@@ -102,7 +102,8 @@ public class TestGameMode : IGameModeBehaviour
             waveCount++;
         }
 
-        // post win event
+        player.isGameActive = false;
+        enemyPlayer.isGameActive = false;
         yield break;
     }
 
