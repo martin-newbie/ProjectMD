c004565e28a080bd72cdee49f6319987fd60153a
martin-newbie <g.martin.min@gmail.com>
2024-02-09 20:27:26 +0900

    [#update] 씬 로드 매니저 추가
    씬 로드, 홈 돌아가기, 이전 씬 로드 기능 사용

---
+++ Assets/Scenes/General/Scripts/SceneLoadManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadManager : MonoBehaviour
+{
+    public static SceneLoadManager Instance;
+    public Stack<string> previousScenes = new Stack<string>();
+
+    string moveSceneName;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void LoadHomeScene()
+    {
+        moveSceneName = "menu";
+        previousScenes.Clear();
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    public void LoadBackScene()
+    {
+        moveSceneName = previousScenes.Pop();
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        moveSceneName = sceneName;
+        previousScenes.Push(SceneManager.GetActiveScene().name);
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        var loadData = SceneManager.LoadSceneAsync(moveSceneName);
+        loadData.allowSceneActivation = false;
+        while (!loadData.isDone)
+        {
+            // scene progress
+            float progress = loadData.progress;
+            yield return null;
+        }
+        loadData.allowSceneActivation = true;
+        yield break;
+    }
+}
