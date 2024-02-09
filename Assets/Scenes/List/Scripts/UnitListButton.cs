4fa69a05163ae18177c35e32bd9425f3a113527d
martin-newbie <g.martin.min@gmail.com>
2024-02-09 21:21:27 +0900

    [#update] 목록 캐릭터 버튼 초기화 시스템 추가

--- Assets/Scenes/List/Scripts/UnitListButton.cs
+++ Assets/Scenes/List/Scripts/UnitListButton.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitListButton : MonoBehaviour
 {
+    [SerializeField] Image unitProfileImg;
+    [SerializeField] Text unitNameTxt;
+
     public void InitButton(UnitData _linkedData)
+    {
+        unitProfileImg.sprite = ResourceManager.GetProfile(_linkedData.index);
+        unitNameTxt.text = StaticDataManager.GetConstUnitData(_linkedData.index).name;
+    }
+
+    public void OnbuttonClick()
     {
 
     }
