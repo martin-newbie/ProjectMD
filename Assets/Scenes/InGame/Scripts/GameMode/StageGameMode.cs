8353d6f96bd33797cbbaa732dcb64e3fc1aa0648
martin-newbie <g.martin.min@gmail.com>
2024-02-18 01:55:04 +0900

    [#update] 스테이지 게임 모드 작동 로직 구성 완료
    이펙트 추가 필요

--- Assets/Scenes/InGame/Scripts/GameMode/StageGameMode.cs
+++ Assets/Scenes/InGame/Scripts/GameMode/StageGameMode.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class StageGameMode : IGameModeBehaviour
@@ -35,11 +36,58 @@ public class StageGameMode : IGameModeBehaviour
 
             player = new PlayableGamePlayer(unitDatas.ToArray(), UnitGroupType.ALLY, manager.skillCanvas);
             isInit = true;
+
+            InGameManager.Instance.StartCoroutine(GameLogic());
         });
     }
 
     public void Update()
     {
+        if (!isInit) return;
 
+        player.Update();
+        enemy.Update();
+
+        if (player.isGameActive && player.curUnits.Count > 0)
+        {
+            var frontUnit = player.curUnits.OrderByDescending(item => item.transform.position.x).ElementAt(0).transform.position;
+            var frontPos = frontUnit.x;
+            var camPos = new Vector3(frontPos + 3, 0f, -10f);
+            mainCam.transform.position = Vector3.Lerp(mainCam.transform.position, camPos, Time.deltaTime * 15f);
+        }
     }
+
+    IEnumerator GameLogic()
+    {
+        // game start effect
+        yield return new WaitForSeconds(1f);
+
+        int waveCount = 0;
+        (player as PlayableGamePlayer).ActiveAllUnits();
+        player.StartGame();
+        enemy.StartGame();
+
+        while (waveCount < 4)
+        {
+            (enemy as TestEnemyGamePlayer).ActiveUnitAt(waveCount);
+            while (enemy.GetCountOfUnits() > 0)
+            {
+                if (player.GetCountOfUnits() <= 0)
+                {
+                    player.isGameActive = false;
+                    // play lose event
+                }
+                yield return null;
+            }
+
+            yield return new WaitUntil((player as PlayableGamePlayer).EveryUnitActionEnds);
+            waveCount++;
+        }
+        
+        // play win event
+        player.isGameActive = false;
+        enemy.isGameActive = false;
+        yield break;
+    }
+
 }
