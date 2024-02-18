854b075867211e9f66b3dfda72aabc21ee32790b
martin-newbie <g.martin.min@gmail.com>
2024-02-18 12:27:08 +0900

    [#update] 헤일라 공격 행동패턴 추가

---
+++ Assets/Scenes/InGame/Scripts/Unit/Ally/Heilra.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heilra : ActiveSkillBehaviour
+{
+    public Heilra(UnitData _unitData, Dictionary<StatusType, float> _statusData) : base(_unitData, _statusData)
+    {
+    }
+
+    public override IEnumerator ActiveSkill(SkillData skillData)
+    {
+        yield break;
+    }
+
+    public override void CollabseBuff(SkillData skillData, UnitBehaviour subjectUnit)
+    {
+
+    }
+
+    protected override IEnumerator AttackLogic()
+    {
+        var target = GetPreferTarget();
+        ShootBullet(target);
+
+        yield return PlayAnimAndWait("battle_attack");
+        yield return PlayAnimAndWait("battle_bolt");
+        yield break;
+    }
+}
