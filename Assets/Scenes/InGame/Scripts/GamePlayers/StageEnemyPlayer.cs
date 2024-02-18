f92e0bbe05e51f21bbf4f67a08f4af4f1f166c22
martin-newbie <g.martin.min@gmail.com>
2024-02-18 14:17:34 +0900

    [#update] 스테이지 적 스탯이 타입에 따라 열화되도록

--- Assets/Scenes/InGame/Scripts/GamePlayers/StageEnemyPlayer.cs
+++ Assets/Scenes/InGame/Scripts/GamePlayers/StageEnemyPlayer.cs
@@ -21,6 +21,23 @@ public class StageEnemyPlayer : GamePlayer
             foreach (var unitData in waveData.unitDatas)
             {
                 var unitStatus = StaticDataManager.GetUnitStatus(unitData.index).GetCalculatedValueDictionary(stageLevel);
+                switch (unitData.unit_type)
+                {
+                    case 0:
+                        unitStatus[StatusType.DMG] /= 2f;
+                        unitStatus[StatusType.HP] /= 3f;
+                        unitStatus[StatusType.DEF] /= 2f;
+                        break;
+                    case 1:
+                        unitStatus[StatusType.HP] /= 2f;
+                        unitStatus[StatusType.DEF] /= 2f;
+                        break;
+                    case 2:
+                        unitStatus[StatusType.DMG] /= 2f;
+                        unitStatus[StatusType.HP] *= 3f;
+                        break;
+                }
+
                 var unit = InGameManager.Instance.SpawnUnit(new Vector3(spawnX, 0), _group, unitData, unitStatus, unitData.unit_type);
                 unit.state = BehaviourState.STANDBY;
                 unit.InjectDeadEvent(() => RemoveActiveUnit(unit));
