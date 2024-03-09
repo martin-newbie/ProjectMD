b0d7a24ff10a6d469bfee855bea63109aaa7fc21
martin-newbie <g.martin.min@gmail.com>
2024-03-09 14:07:17 +0900

    [#update] 결과창 유닛 정보 갱신

--- Assets/Scenes/InGame/Scripts/UI/Result/ResultUnitInfo.cs
+++ Assets/Scenes/InGame/Scripts/UI/Result/ResultUnitInfo.cs
@@ -6,22 +6,34 @@ using UnityEngine.UI;
 
 public class ResultUnitInfo : MonoBehaviour
 {
-    public SkeletonGraphic model;
+    [SerializeField] SkeletonGraphic model;
+    [SerializeField] Image prevExpGauge;
+    [SerializeField] Image nextExpGauge;
+    [SerializeField] Text prevLevelText;
+    [SerializeField] Text nextLevelText;
+    [SerializeField] Text updatedExpText;
+    [SerializeField] Text flavorText;
 
-    public Image prevExpGauge;
-    public Image nextExpGauge;
-
-    public Text prevLevelText;
-    public Text nextLevelText;
-    public Text updatedExpText;
-
-    public Text flavorText;
-
-    public void InitUnitInfo(UnitData unitData)
+    public void InitUnitInfo(UnitData unitData, int prevLevel, int prevExp, int updatedExp)
     {
         var modelIndex = StaticDataManager.GetConstUnitData(unitData.index).modelIndex;
         var spineAsset = ResourceManager.GetSkeleton(modelIndex);
         model.UpdateSkeleton(spineAsset);
 
+        if(prevLevel < unitData.level)
+        {
+            prevExpGauge.fillAmount = 0f;
+        }
+        else
+        {
+            prevExpGauge.fillAmount = (float)prevExp / (float)ResourceManager.GetUnitLevelupExp(prevLevel);
+        }
+
+        prevLevelText.text = unitData.level.ToString();
+        nextLevelText.text = (unitData.level + 1).ToString();
+        nextExpGauge.fillAmount = (float)unitData.exp / (float)ResourceManager.GetUnitLevelupExp(unitData.level);
+        updatedExpText.text = updatedExp.ToString();
+        updatedExpText.rectTransform.anchoredPosition = new Vector2(225f * nextExpGauge.fillAmount, 20.5f);
+        flavorText.text = "";
     }
 }
