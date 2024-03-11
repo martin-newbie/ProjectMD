0bb77d24b965d6e862be0660a36f5e1004e70c92
martin-newbie <g.martin.min@gmail.com>
2024-03-12 02:18:56 +0900

    [#fix] 불필요 오류 유발 파라미터 제거

--- Assets/Scenes/Overall/Scripts/OverallManager.cs
+++ Assets/Scenes/Overall/Scripts/OverallManager.cs
@@ -9,22 +9,10 @@ public class OverallManager : MonoBehaviour
     [SerializeField] OverallStatusPanel statusPanel;
     [SerializeField] SkeletonGraphic unitModel;
 
-    [Header("Summary")]
-    [SerializeField] Text nameText;
-    [SerializeField] Text positionText;
-    [SerializeField] Image positionImage;
-    [SerializeField] Text atkTypeText;
-    [SerializeField] Image atkTypeImage;
-    [SerializeField] Text defTypeText;
-    [SerializeField] Image defTypeImage;
-
     private void Start()
     {
         int unitId = TempData.Instance.selectedUnit;
         var unitData = UserData.Instance.FindUnitWithId(unitId);
-        var unitConstData = StaticDataManager.GetConstUnitData(unitData.index);
-
-        nameText.text = unitConstData.name;
 
         unitModel.UpdateSkeleton(ResourceManager.GetSkeleton(unitData.index));
         unitModel.AnimationState.SetAnimation(0, "battle_wait", true);
