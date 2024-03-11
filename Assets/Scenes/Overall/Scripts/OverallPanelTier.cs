78b34658157a11bf93a69ba55ae93280e6ad47ff
martin-newbie <g.martin.min@gmail.com>
2024-03-12 02:14:42 +0900

    [#update] 계급 성장 오버롤 패널 스크립트만 만들어서 적용

---
+++ Assets/Scenes/Overall/Scripts/OverallPanelTier.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverallPanelTier : MonoBehaviour, IOverallPanel
+{
+    public void Open(UnitData data)
+    {
+    }
+
+}
