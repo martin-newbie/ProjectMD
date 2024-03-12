f421fff736088d4230592a72143f1e5779bea5ff
martin-newbie <g.martin.min@gmail.com>
2024-03-12 20:13:12 +0900

    [#update] 테스트 아이템 획득 패널 생성

---
+++ Assets/Scenes/Tests/ItemTest/Scripts/TestItemCanvas.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestItemCanvas : MonoBehaviour
+{
+    public void GetItem(int idx)
+    {
+        var item = new ItemData();
+        item.idx = idx;
+        item.count = 1;
+        WebRequest.Post("user/get-item", JsonUtility.ToJson(item), (data) =>
+        {
+            var recieve = JsonUtility.FromJson<RecieveGetItem>(data);
+
+        });
+    }
+}
+
+public class RecieveGetItem
+{
+
+}
