0819150fe77818c5326fd39b83ad395dbb662b9b
martin-newbie <g.martin.min@gmail.com>
2024-03-16 02:38:34 +0900

    [#update] 시트 구성 변경

--- Assets/Scenes/General/Scripts/StaticDatas/Scripts/StaticUnitSkillStatus.cs
+++ Assets/Scenes/General/Scripts/StaticDatas/Scripts/StaticUnitSkillStatus.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [CreateAssetMenu(fileName = "StaticUnitSkillStatus", menuName = "MD/ScriptableData/StaticUnitSkillStatus", order = int.MinValue)]
 public class StaticUnitSkillStatus : SheetDataBase
@@ -30,22 +31,10 @@ public class UnitSkillStatus
     public int cost;
     public int skill_type;
 
-    public float active_min;
-    public float active_max;
-    public float active_assist_1;
-    public float active_assist_2;
-
-    public float passive_min;
-    public float passive_max;
-    public float passive_assist;
-
-    public float enforce_min;
-    public float enforce_max;
-    public float enforce_assist;
-
-    public float sub_min;
-    public float sub_max;
-    public float sub_assist;
+    public float[] active;
+    public float[] passive;
+    public float[] enforce;
+    public float[] sub;
 
     public UnitSkillStatus(string[] data)
     {
@@ -55,41 +44,29 @@ public class UnitSkillStatus
         cost = int.Parse(data[idx++]);
         skill_type = int.Parse(data[idx++]);
 
-        active_min = float.Parse(data[idx++]);
-        active_max = float.Parse(data[idx++]);
-        active_assist_1 = float.Parse(data[idx++]);
-        active_assist_2 = float.Parse(data[idx++]);
-
-        passive_min = float.Parse(data[idx++]);
-        passive_max = float.Parse(data[idx++]);
-        passive_assist = float.Parse(data[idx++]);
-
-        enforce_min = float.Parse(data[idx++]);
-        enforce_max = float.Parse(data[idx++]);
-        enforce_assist = float.Parse(data[idx++]);
-
-        sub_min = float.Parse(data[idx++]);
-        sub_max = float.Parse(data[idx++]);
-        sub_assist = float.Parse(data[idx++]);
+        active = data[idx++].Split(',').Select(item => float.Parse(item)).ToArray();
+        passive = data[idx++].Split(',').Select(item => float.Parse(item)).ToArray();
+        enforce = data[idx++].Split(',').Select(item => float.Parse(item)).ToArray();
+        sub = data[idx++].Split(',').Select(item => float.Parse(item)).ToArray();
     }
 
     public float GetActiveSkillValue(int skillLevel)
     {
-        return ((active_max - active_min) / 5 * skillLevel) + active_min;
+        return ((active[1] - active[0]) / 5 * skillLevel) + active[0];
     }
 
     public float GetPassiveSkillValue(int skillLevel)
     {
-        return ((passive_max - passive_min) / 5 * skillLevel) + passive_min;
+        return ((passive[1] - passive[0]) / 10 * skillLevel) + passive[0];
     }
 
     public float GetEnforceSkillValue(int skillLevel)
     {
-        return ((enforce_max - enforce_min) / 5 * skillLevel) + enforce_min;
+        return ((enforce[1] - enforce[0]) / 10 * skillLevel) + enforce[0];
     }
     
     public float GetSubSKillValue(int skillLevel)
     {
-        return ((sub_max - sub_min) / 5 * skillLevel) + sub_min;
+        return ((sub[1] - sub[0]) / 10 * skillLevel) + sub[0];   
     }
 }
